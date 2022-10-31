9c79bad652027c1f3543fa40cc79e707ca10f3d2
mauger1998 <110930037+mauger1998@users.noreply.github.com>
2022-10-31 06:52:15 +0000

    Add error type animation after load

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const progressBar = document.getElementsByClassName("progressBar")[0]
+const mainSection = document.querySelector("main")
+
+
+
+
 setInterval(() => {
     const computedStyle = getComputedStyle(progressBar)
     const width = parseFloat(computedStyle.getPropertyValue("--width")) || 0
     progressBar.style.setProperty("--width", width + .1)
     if (width == 100) {
-        h1.classList.add("add")
+        mainSection.classList.add("classToAdd")
+        progressBar.style.display = "none"
     }
 }, 5)
